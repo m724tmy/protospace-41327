60426aadd9fed021880301c0fe8a8c2a1f10bdce
m724tmy <tomoya.724.a7x@icloud.com>
2024-09-10 18:29:00 +0900

    エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -18,7 +18,7 @@ class PrototypesController < ApplicationController
   def edit
     @prototype = Prototype.find(params[:id])
     unless current_user.id == @prototype.user_id
-      redirect_to action: :index
+      redirect_to action: :show
     end
   end
 
